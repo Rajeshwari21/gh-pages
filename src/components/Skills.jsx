4ad82818271323122e94b4370429a486ae9097ab
Rajeshwari <rvariar10@gmail.com>
2022-02-07 21:23:16 -0800

    Made changes to testing files.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,78 +1,79 @@
 import React, { Component } from "react";
 
-// import pyimg from "../components/images/pythonimg.png";
-// import jsimg from "../components/images/javascriptimg2.png";
-// import htmlimg from "../components/images/html5logo.jpg";
-// import cssimg from "../components/images/csslogo.png";
-// import mongoimg from "../components/images/mngodbimg.png";
-// import databasebimg from "../components/images/database.jpg";
-// import java from "../components/images/java.png";
-// import php from "../components/images/php.png";
+import pyimg from "../components/images/pythonimg.png";
+import jsimg from "../components/images/javascriptimg2.png";
+import htmlimg from "../components/images/html5logo.jpg";
+import cssimg from "../components/images/csslogo.png";
+import mongoimg from "../components/images/mngodbimg.png";
+import databasebimg from "../components/images/database.jpg";
+import java from "../components/images/java.png";
+import php from "../components/images/php.png";
 
 class Skills extends Component {
   render() {
     return (
-      <section id="skillheader" className="flex-project-container"></section>
-      //   <div>
-      //     <img src={java} width="100" height="100" alt="java" title="java" />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={pyimg}
-      //       width="100"
-      //       height="100"
-      //       alt="python"
-      //       title="python"
-      //     />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={jsimg}
-      //       width="100"
-      //       height="100"
-      //       alt="js"
-      //       title="javascript"
-      //     />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={htmlimg}
-      //       width="100"
-      //       height="100"
-      //       alt="html"
-      //       title="html5"
-      //     />
-      //   </div>
-      //   <div>
-      //     <img src={cssimg} width="100" height="100" alt="css" title="css" />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={mongoimg}
-      //       width="100"
-      //       height="100"
-      //       alt="mongodb"
-      //       title="mongodb"
-      //     />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={databasebimg}
-      //       width="100"
-      //       height="100"
-      //       alt="db"
-      //       title="database"
-      //     />
-      //   </div>
-      //   <div>
-      //     <img
-      //       src={php}
-      //       width="100"
-      //       height="100"
-      //       alt="php"
-      //       title="php"
-      //     />
-      //   </div>
+      <section id="skillheader" className="flex-project-container">
+        <div>
+          <img src={java} width="100" height="100" alt="java" title="java" />
+        </div>
+        <div>
+          <img
+            src={pyimg}
+            width="100"
+            height="100"
+            alt="python"
+            title="python"
+          />
+        </div>
+        <div>
+          <img
+            src={jsimg}
+            width="100"
+            height="100"
+            alt="js"
+            title="javascript"
+          />
+        </div>
+        <div>
+          <img
+            src={htmlimg}
+            width="100"
+            height="100"
+            alt="html"
+            title="html5"
+          />
+        </div>
+        <div>
+          <img src={cssimg} width="100" height="100" alt="css" title="css" />
+        </div>
+        <div>
+          <img
+            src={mongoimg}
+            width="100"
+            height="100"
+            alt="mongodb"
+            title="mongodb"
+          />
+        </div>
+        <div>
+          <img
+            src={databasebimg}
+            width="100"
+            height="100"
+            alt="db"
+            title="database"
+          />
+        </div>
+        <div>
+          <img
+            src={php}
+            width="100"
+            height="100"
+            alt="php"
+            title="php"
+          />
+        </div>
+      </section>
     );
   }
 }
